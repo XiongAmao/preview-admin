45c9274ed4281f2cd2e05d97dff3a0b8553ef1c4
XiongAmao <onycat@live.cn>
2018-08-02 18:11:27 +0800

    :fire: remove create-react-app build

--- site/preview-admin/config-overrides.js
+++
@@ -1,6 +0,0 @@
-const { injectBabelPlugin } = require('react-app-rewired')
-
-module.export = function override(config, env) {
-  config = injectBabelPlugin(['import', { libraryName: 'antd', libraryDirectory: 'es', style: 'css', }], config) 
-  return config
-}
