78f70035059e4cd3a58d0c0684f76eb25229b39f
XiongAmao <onycat@live.cn>
2018-08-19 12:24:57 +0800

    :sparkles:  schedule save rp-list to mongodb

--- server/schedule/rp_schedule.js
+++ server/schedule/rp_schedule.js
@@ -1,13 +1,11 @@
 const schedule = require('node-schedule')
-const RpListModel = require('../db/model/rp_list_model')
-const { getPreviewList } = require('../libs/util')
-const rpEntry = require('../config').rpDirPath
+const { updateRPList } = require('../service/rp_service')
 
 const rpSchedule = () => {
   const rp = schedule.scheduleJob('30 * * * * *', function () {
-    console.time('getPreviewList')
-    const list = getPreviewList(rpEntry)
-    console.timeEnd('getPreviewList')
+    updateRPList().then(() => {
+      console.log('[info] rp-list updated')
+    })
   })
 }
 
--- server/service/rp_service.js
+++ server/service/rp_service.js
@@ -2,12 +2,46 @@ const { getPreviewList } = require('../libs/util')
 const RpListModel = require('../db/model/rp_list_model')
 const rpEntry = require('../config').rpDirPath
 
-const cacheRPList = () => {
-  console.time('getPreviewList')
-  const list = getPreviewList(rpEntry)
-  console.timeEnd('getPreviewList')
+const removeRpItemsByPathList = (list) => {
+  return new Promise((resolve, reject) => {
+    if (list.length === 0) return resolve()
+    RpListModel.remove({ path: { $in: list } }, (err) => {
+      if (err) reject(err)
+      else resolve()
+    })
+  })
+}
+
+const createRpItemByList = (list) => {
+  return new Promise((resolve, reject) => {
+    if (list.length === 0) return resolve()
+    RpListModel.create(list, (err, list) => {
+      if (err) reject(err)
+      else resolve()
+    })
+  })
+}
+
+const updateRPList = () => {
+  const localList = getPreviewList(rpEntry)
+  return new Promise((resolve, reject) => {
+    RpListModel.find({}, (err, dbList) => {
+      if (err) reject(err)
+      // 获取目录中不存在的项目，用于移除，处理成path数组
+      const needRemoveList = dbList
+        .filter(dbItem => localList.every(localItem => dbItem.path !== localItem.path))
+        .map(item => item.path)
+
+      const needUpdateList = localList
+        .filter(localItem => dbList.every(dbItem => dbItem.path !== localItem.path))
+
+      Promise.all([removeRpItemsByPathList(needRemoveList), createRpItemByList(needUpdateList)])
+        .then(() => resolve())
+        .catch(err => reject(err))
+    })
+  })
 }
 
 module.exports = {
-  cacheRPList
+  updateRPList
 }
