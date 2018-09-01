e34b98635c1063a7815f02aeb18ecc508e54af2a
XiongAmao <onycat@live.cn>
2018-09-01 16:58:41 +0800

    :fire: remove temp data & add .gitignore

--- server/db/model/session_model.js
+++ server/db/model/session_model.js
@@ -3,5 +3,5 @@ const SessionSchema = require('../schema/session_schema')
 const SessionModel = mongoose.model('session', SessionSchema)
 // The first argument is the singular name of the collection your model is for. Mongoose automatically looks for the plural version of your model name
 // Thus the collection name is setted in schema.
-
+add some thing
 module.exports = SessionModel
