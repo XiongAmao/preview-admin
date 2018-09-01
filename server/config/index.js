50ce799454290bb0144426418d14b3016ed64b09
XiongAmao <onycat@live.cn>
2018-09-01 17:26:19 +0800

    change config path

--- server/config/index.js
+++ server/config/index.js
@@ -1,8 +1,26 @@
-module.exports = {
+const fs = require('fs')
+const path = require('path')
+
+const resolve = (p) => {
+  return path.resolve(__dirname, p)
+}
+
+let customConfig = null
+let customConfigPath = resolve('../../config.js')
+
+const defaultConfig = {
   port: 9992,
-  rpDirPath: '/Users/xiongzc/repos/self-projects/preview-admin/data/rp', // 使用服务器绝对路径
+  rpDirPath: resolve('../../data'), // temp path, you have to use absolute path in production.
   sketchDirPath: '',
   mongodb: 'mongodb://127.0.0.1:27017/tools-admin',
   MD5_SUFFIX: '很长的盐', // salt
   secretKey: 'test-admin-secretKey'
 }
+
+if (fs.existsSync(customConfigPath)) {
+  customConfig = require(customConfigPath)
+}
+
+const config = Object.assign({}, defaultConfig, customConfig)
+
+module.exports = config
